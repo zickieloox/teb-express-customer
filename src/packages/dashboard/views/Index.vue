2a9c4fcc7a83129bd6373d604996f5b97699b9f4
ミ★ŤɦαŋɦŤùŋɠ★彡 <tungnbt@lionnix.vn>
2021-06-10 13:35:34 +0700

    fix link

--- src/packages/dashboard/views/Index.vue
+++ src/packages/dashboard/views/Index.vue
@@ -1,7 +1,21 @@
 <template>
   <div>
     <div class="order-view-page pages">
-      <div class="container"> </div>
+      <div class="container">
+        <button @click="hadel"></button>
+      </div>
     </div>
   </div>
 </template>
+<script>
+export default {
+  methods: {
+    hadel() {
+      this.$toast.open({
+        type: 'error',
+        message: 'You are not allowed to do this action !',
+      })
+    },
+  },
+}
+</script>
