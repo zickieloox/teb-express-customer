7a10c421d75027bedb61d2764bfc7a2221318395
hung.dh2903 <hung.dh2903@gmail.com>
2021-07-09 14:57:38 +0700

    fix admin rep

--- src/packages/claim/constants.js
+++ src/packages/claim/constants.js
@@ -6,8 +6,7 @@ export const CLAIM_STATUS = {
 export const CLAIM_STATUS_PENDING = 1
 export const CLAIM_STATUS_PROCESSED = 2
 
-export const CLAIM_INIT = 0
+export const CLAIMCLAIM_CUSTOMER_REPLY_INIT = 0
 export const CLAIM_ADMIN_REPLY = 1
-export const CLAIM_CUSTOMER_REPLY = 2
 
 export const MAXIMUM_SIZE = 5
