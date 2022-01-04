414a7232d556fcf83db6127155d2b49655cb5397
tungnbt <tungnbt@lionnix.vn>
2022-01-04 13:12:43 +0700

    remove if tracking

--- src/packages/package/views/PackageDetail.vue
+++ src/packages/package/views/PackageDetail.vue
@@ -753,10 +753,7 @@ export default {
     },
 
     isOverThanOld(prop) {
-      if (
-        !this.package_detail.package ||
-        !this.package_detail.package.tracking
-      ) {
+      if (!this.package_detail.package) {
         return false
       }
 
