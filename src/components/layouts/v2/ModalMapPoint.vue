49b899366d5a6d444f05b7ff952865ac611890e2
dzungnt2412 <dzungnt2412@gmail.com>
2022-04-15 01:48:51 +0700

    fix

--- src/components/layouts/v2/ModalMapPoint.vue
+++ src/components/layouts/v2/ModalMapPoint.vue
@@ -23,7 +23,7 @@
         >
         </multiselect>
       </div>
-      <div class="list-warehouse">
+      <div class="list-warehouse" v-if="!isLoading">
         <div
           class="warehouse-item "
           v-for="(warehouse, i) in warehousesDTO"
@@ -79,6 +79,9 @@
           </div>
         </div>
       </div>
+      <div class="is-loading" v-else>
+        <img src="@assets/img/loading.gif" />
+      </div>
     </div>
   </p-modal>
 </template>
@@ -106,6 +109,7 @@ export default {
       uploading: true,
       search: '',
       warehousesDTO: [],
+      isLoading: true,
     }
   },
 
@@ -136,6 +140,7 @@ export default {
     },
 
     async init() {
+      this.isLoading = true
       let payload = { type: 2, status: 1 }
       const result = await this[FETCH_WAREHOUSES](payload)
       if (!result.success) {
@@ -146,23 +151,30 @@ export default {
       let options = []
 
       this.warehouses.forEach((warehouse) => {
-        options.push({ name: warehouse.city })
+        options.push(warehouse.city)
       })
 
-      options = options.concat(this.optionsCity)
+      options = options.filter((element, index) => {
+        return options.indexOf(element) == index
+      })
 
-      this.optionsCity = options
+      options.forEach((item) => {
+        this.optionsCity.push({ name: item })
+      })
 
       this.warehousesDTO = this.warehouses
+
+      this.isLoading = false
     },
 
     handleSearch() {
+      let city = this.city.name
       this.warehousesDTO = this.warehouses.filter((warehouse) =>
         this.validSearch(warehouse.address).includes(this.search.toUpperCase())
       )
-      if (this.city != '') {
+      if (city != '') {
         this.warehousesDTO = this.warehouses.filter((warehouse) =>
-          this.validSearch(warehouse.city).includes(this.city.toUpperCase())
+          this.validSearch(warehouse.city).includes(city.toUpperCase())
         )
       }
     },
