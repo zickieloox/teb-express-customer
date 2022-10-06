675825ebb06fea6cbe9c397c12cfddcc914ac0ca
Van Hanh <hanhtv@lionnix.vn>
2022-10-06 14:04:44 +0700

    superset ty gia

--- src/packages/home/views/Home.vue
+++ src/packages/home/views/Home.vue
@@ -67,6 +67,20 @@
                     </div>
                   </div>
                 </div>
+                <div class="col-12 mt-24">
+                  <div class="superset">
+                    <iframe
+                      style="overflow: hidden; width: 100%; height: 100vh"
+                      width="600"
+                      height="1500"
+                      seamless
+                      frameBorder="0"
+                      scrolling="yes"
+                      :src="supersetLink"
+                    >
+                    </iframe>
+                  </div>
+                </div>
               </div>
             </div>
             <!--            <div class="col-3 list-actions">-->
@@ -212,6 +226,9 @@ export default {
       }
 
       return days
+    },
+    supersetLink() {
+      return `https://superset-new.lionnix.com/login?username=view&redirect=${process.env.VUE_APP_SUPERSET_DASHBOARD}`
     },
   },
   mounted() {
@@ -367,3 +384,9 @@ export default {
   },
 }
 </script>
+<style>
+.superset {
+  margin-left: -32px;
+  margin-right: -32px;
+}
+</style>
