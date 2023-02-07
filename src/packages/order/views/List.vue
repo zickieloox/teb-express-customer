05df652745c7478d5cd0ef47eab8c75aea2db082
Van Hanh <hanhtv@lionnix.vn>
2023-02-07 14:25:54 +0700

    export name order

--- src/packages/order/views/List.vue
+++ src/packages/order/views/List.vue
@@ -241,7 +241,7 @@ export default {
         return
       }
 
-      this.downloadFile(res.path, 'packages', '', `kien_hang_${id}_`)
+      this.downloadFile(res.path, 'packages', '', `kien_hang_${id}_date`)
     },
   },
   watch: {
