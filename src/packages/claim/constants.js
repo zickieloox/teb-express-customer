a27e640ba74ef489edcdb32a449111ac69ee91d6
khoand <neu.khoanguyen@gmail.com>
2023-05-23 09:56:11 +0700

    update view

--- src/packages/claim/constants.js
+++ src/packages/claim/constants.js
@@ -1,12 +1,15 @@
-export const CLAIM_STATUS_PENDING = 1
-export const CLAIM_STATUS_PROCESSED = 2
+const CLAIM_STATUS_APPLYING = 1
+export const CLAIM_STATUS_PENDING = 2
+export const CLAIM_STATUS_PROCESSED = 3
 
 export const CLAIM_STATUS = {
+  'Xét duyệt': CLAIM_STATUS_APPLYING,
   'Đang xử lý': CLAIM_STATUS_PENDING,
   'Đã xử lý': CLAIM_STATUS_PROCESSED,
 }
 
 export const CLAIM_STATUS_TEXT = {
+  [CLAIM_STATUS_APPLYING]: 'Xét duyệt',
   [CLAIM_STATUS_PENDING]: 'Đang xử lý',
   [CLAIM_STATUS_PROCESSED]: 'Đã xử lý',
 }
@@ -17,6 +20,10 @@ export const CLAIM_ADMIN_REPLY = 1
 export const MAXIMUM_SIZE = 5
 
 export const MAP_CLAIM_STATUS = {
+  [CLAIM_STATUS_APPLYING]: {
+    text: 'Xét duyệt',
+    className: 'pending',
+  },
   [CLAIM_STATUS_PENDING]: {
     text: 'Đang xử lý',
     className: 'pending',
