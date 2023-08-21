3ff07be10954098a39981cf66304f1d79b01d027
khoand <neu.khoanguyen@gmail.com>
2023-08-21 09:04:59 +0700

    update

--- src/packages/package/views/components/ModalCoupon.vue
+++ src/packages/package/views/components/ModalCoupon.vue
@@ -61,13 +61,20 @@
                 ></inline-svg>
               </div>
               <div class="txt">
+                <div class="d-flex">
+                  <span class="cp-code" :title="item.code">{{
+                    item.code
+                  }}</span>
+                  <span class="cp-count"
+                    >Còn {{ item.quantity - item.used }} Coupons</span
+                  >
+                </div>
                 <h2>{{ item.text_head }}</h2>
-                <p>Số lượng: {{ item.quantity }}</p>
-                <p>Ngày hết hạn: {{ item.end_date | date('dd/MM/yyyy') }}</p>
                 <p v-if="isDiscount(item.type)">
                   Giá trị áp dụng tối thiểu:
                   {{ item.min_apply | formatPrice }}</p
                 >
+                <p>HSD: {{ item.end_date | date('dd/MM/yyyy') }}</p>
               </div>
               <div class="action">
                 <p-radio
